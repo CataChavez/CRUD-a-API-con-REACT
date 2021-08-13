406e70871887867a47c6f6bf7aa880bf3759406c
Catty <kata.chavez@gmail.com>
2021-08-12 21:53:12 -0400

    get-user and update-user add

--- src/store/users/reducer.js
+++ src/store/users/reducer.js
@@ -2,6 +2,12 @@ import {
   FETCH_USERS_START,
   FETCH_USERS_SUCCESS,
   FETCH_USERS_FAILURE,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILURE,
+  UPDATE_USER_REQUEST,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_FAILURE,
 } from "./constants";
 
 const initialState = {
@@ -31,6 +37,44 @@ const usersReducer = (state = initialState, action) => {
         isFetching: false,
         data: [],
       };
+    case GET_USER_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case GET_USER_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+      }
+    case GET_USER_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload,
+        isFetching: false,
+        data: [],
+      }
+    case UPDATE_USER_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case UPDATE_USER_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        data: action.payload,
+      }
+    case UPDATE_USER_FAILURE:
+      return {
+        ...state,
+        errorMessage: action.payload,
+        isFetching: false,
+        data: [],
+      }
+    
+
     default:
       return state;
   }
