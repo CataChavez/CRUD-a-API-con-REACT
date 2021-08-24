43f61ec0013cef1d16f7f4992fa6fc6b1ac78012
Catty <kata.chavez@gmail.com>
2021-08-23 20:02:20 -0400

    remove mode 'no-cors'

--- src/store/users/thunks.js
+++ src/store/users/thunks.js
@@ -22,7 +22,6 @@ export const fetchUsersStartThunk = () => {
         dispatch(fetchUsersStart());
         try {
             const response = await fetch("https://jsonplaceholder.typicode.com/users",{
-                mode: 'no-cors'
             });
             const data = await response.json();
             dispatch(fetchUsersSucces(data));
